8f01684653f8109a9eb54fb3596a20a9a5c719c0
Lenssen <manasae.lenssen@gmail.com>
2020-06-19 10:03:07 +0530

    Random

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -1,7 +1,12 @@
+import random
 user_input = str(input())
-if user_input == 'scissors':
-    print('Sorry, but computer chose rock')
-elif user_input == 'paper':
-    print('Sorry, but computer chose scissors')
-elif user_input == 'rock':
-    print('Sorry, but computer chose paper')
+str = ['rock','paper','scissors']
+computer_input = random.choice(str)
+winners = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
+
+if user_input == computer_input :
+    print(f'There is a draw({user_input})')
+elif winners[user_input] == computer_input:
+    print(f'Well done. Computer chose {computer_input} and failed')
+else:
+    print(f'Sorry, but computer chose {computer_input}')
