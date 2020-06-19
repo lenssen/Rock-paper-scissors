059037bbfaf3d04f345ee21e85b5c44372236671
Lenssen <manasae.lenssen@gmail.com>
2020-06-19 09:23:37 +0530

    unfair game

---
+++ rock-paper-scissor.py
@@ -0,0 +1,7 @@
+user_input = str(input())
+if user_input == 'scissors':
+    print('Sorry, but computer chose rock')
+elif user_input == 'paper':
+    print('Sorry, but computer chose scissors')
+elif user_input == 'rock':
+    print('Sorry, but computer chose paper')
